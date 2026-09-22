04780239f047a9decedee114246a1d09d267d247
nalayyou <alayyoubnoorhan@gmail.com>
2026-09-22 16:38:36 +0300

    montior is finished for now

--- src/monitor.c
+++ src/monitor.c
@@ -1,16 +1,31 @@
 #include "codexion.h"
 
-void* monitor(void* config)
+void* monitor(void* uncasted_config)
 {
+    t_config *config = (t_config*)uncasted_config;
+    int all_compiled;
+    all_compiled = 1;
     while(1)
     {
         int i;
         i = 0;
+        all_compiled = 1;
         while(i < config->number_of_coders)
         {
             if(config->time_to_burnout == get_time_ms() -config->coders[i].time_of_last_compile ){
-                printf("a coder burned out\n");
+                printf("coder %d burned out\n", i);
                 config->state_of_sim = 0;
-                return
+                return NULL;
+            }
+            if(config->coders[i].compiles_left)
+                all_compiled = 0;
+            i++;
         }
+        if(all_compiled){
+            config->state_of_sim = 1;
+            return NULL;
+        }
+
+
+    }
 }
