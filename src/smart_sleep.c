6bf24b289490e3ba7d368cf0446baa818ae0d395
nalayyou <alayyoubnoorhan@gmail.com>
2026-09-23 17:57:41 +0300

    smart sleep is finished

--- src/smart_sleep.c
+++ src/smart_sleep.c
@@ -8,6 +8,8 @@ void smart_sleep(t_coder* coder, long long time_in_ms)
     started = get_time_ms();
     while( get_time_ms() - started < time_in_ms){
     usleep(500); // usleep is jsut sleep in microseconds
-    //check if anyone got burned in hell
+    pthread_mutex_lock(&coder->config->simulation_mutex);
+    if (!coder->config->state_of_sim)
+        return;
     }
 }
